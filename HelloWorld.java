4ce1be4d84ef41e5255eb3103efdfcf73c16adc6
Nitesh Dudhe <niteshdudhe13@gmail.com>
2026-04-27 09:05:27 +0530

    Add print statements for Jenkins pipeline
    Added extra print statements for Jenkins pipeline.

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         // This line prints the message to the console
         System.out.println("Hello, World!");
-	System.out.println("This is the extra line of code");
+	    System.out.println("This is the extra line of code");
         System.out.println("This is Nitesh.");
+		System.out.println("For the Jenkins pipeline")
     }
 }
